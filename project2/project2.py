66bb2488fb3e8fca1539d2afa18e831c717cc0f3
JunlinWang <12112921@mail.sustech.edu.cn>
2023-06-18 17:15:36 +0800

    Update project2.py

--- project2/project2.py
+++ project2/project2.py
@@ -3,12 +3,12 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_validate
 from sklearn import svm
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+from sklearn.metrics import confusion_matrix
 from collections import Counter
 from statsmodels.stats.outliers_influence import variance_inflation_factor
 
@@ -37,13 +37,16 @@ def load_data():
     y = data[41]
     del data[41]
     X = data
+    # Further processing
     X = np.array(X)
     X = StandardScaler().fit_transform(X)  # Standardize the observations
     y = np.array(y)
     y = np.nan_to_num(y)
+    print(X.shape)
+    print(y.shape)
     return X, y
 
-# 定义一个函数，用于检测异常值
+# 检测异常值
 def detect_outliers(df, n, features):
     outlier_indices = []
     # 遍历每个特征
@@ -65,133 +68,124 @@ def detect_outliers(df, n, features):
     multiple_outliers = list(k for k, v in outlier_indices.items() if v > n)
     return multiple_outliers
 
+# 数据清洗
+def data_cleaning(X):
+    X.dropna(inplace=True)
+    X.drop_duplicates(inplace=True)
+    outliers_to_drop = detect_outliers(X, 4, X.columns)
+    X.drop(outliers_to_drop, inplace=True)
 
-X, y = load_data()
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)  # split the dataset into training dataset and testing dataset
+# 特征选择
+def feature_selection(X):
+    # 方法1：截距项
+    name = X.columns
+    X = np.matrix(X)
+    print(X)
+    VIF_list = [variance_inflation_factor(X,i) for i in range(X.shape[1])]
+    VIF = pd.DataFrame({'feature':name,"VIF":VIF_list})
+    print(VIF)
+    # 方差膨胀系数VIF大于10时表示特征之间有较高的共线性
 
-# =======================
-# =======数据清洗========
-# =======================
+    # 方法2：person相关系数
+    corr_df = X.corr()
+    # 热力图
+    sns.heatmap(corr_df)
+    plt.show()
+    # 剔除相关性系数高于threshold的corr_drop
+    threshold = 0.9
+    upper = corr_df.where(np.triu(np.ones(corr_df.shape), k=1).astype(np.bool_))
+    corr_drop = [column for column in upper.columns if any(upper[column].abs() > threshold)]
+    print(corr_drop)
+    
 
-X = pd.DataFrame(X)
-y = pd.DataFrame(y)
-X.dropna(inplace=True)
-X.drop_duplicates(inplace=True)
-outliers_to_drop = detect_outliers(X, 4, X.columns)
-X.drop(outliers_to_drop, inplace=True)
+if __name__ == '__main__':
+    # Loading data
+    X, y = load_data()
+    # Feature selection
+    feature_selection(X)
+    # Split the dataset into training dataset and testing dataset
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    
+    ''' Training different models ''' 
+    # 1. KNN
+    KNN = KNeighborsClassifier(n_neighbors=2, algorithm='kd_tree')
+    KNN.fit(X_train, y_train)
+    # Evaluating the model
+    knn_scores = cross_validate(KNN, X_train, y_train, scoring=['precision_macro', 'recall_macro', 'f1_macro', 'accuracy'])
+    print('knn_accuracy: ', knn_scores['test_accuracy'].mean())
+    print('knn_macro_precision: ', knn_scores['test_precision_macro'].mean())
+    print('knn_macro_recall: ', knn_scores['test_recall_macro'].mean())
+    print('knn_macro_f1_score: ', knn_scores['test_f1_macro'].mean())
+    print('\n')
+    # Obtaining the confusion matrix
+    knn_conf_matrix = confusion_matrix(y_test, KNN.predict(X_test))
+    knn_conf_matrix = pd.DataFrame(knn_conf_matrix, index=['Normal', 'DOS', 'Probing', 'R2L', 'U2R'], columns=['Normal', 'DOS', 'Probing', 'R2L', 'U2R']) 
+    sns.heatmap(knn_conf_matrix, annot=True)
+    plt.ylabel('True Labels', fontsize=14)
+    plt.xlabel('Predictions', fontsize=14)
+    plt.title('KNN Confusion Matrix')
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+    plt.show()
 
-# =======================
-# =最终获取能够使用的X和y==
-# =======================
-X = np.array(X)
-X = StandardScaler().fit_transform(X)
-y = np.array(y)
-y = np.nan_to_num(y)
-print(X)
-print(y)
+    # 2. Decision Tree
+    Decision_Tree = DecisionTreeClassifier(criterion='entropy', random_state=42, max_depth=5000)
+    Decision_Tree.fit(X_train, y_train)
+    # Evaluating the model
+    decision_tree_scores = cross_validate(Decision_Tree, X_train, y_train, scoring=['precision_macro', 'recall_macro', 'f1_macro', 'accuracy'])
+    print('decision_tree_accuracy: ', decision_tree_scores['test_accuracy'].mean())
+    print('decision_tree_macro_precision: ', decision_tree_scores['test_precision_macro'].mean())
+    print('decision_tree_macro_recall: ', decision_tree_scores['test_recall_macro'].mean())
+    print('decision_tree_macro_f1_score: ', decision_tree_scores['test_f1_macro'].mean())
+    print('\n')
+    # Obtaining the confusion matrix
+    decision_tree_conf_matrix = confusion_matrix(y_test, Decision_Tree.predict(X_test))
+    decision_tree_conf_matrix = pd.DataFrame(decision_tree_conf_matrix, index=['Normal', 'DOS', 'Probing', 'R2L', 'U2R'], columns=['Normal', 'DOS', 'Probing', 'R2L', 'U2R'])  
+    sns.heatmap(decision_tree_conf_matrix, annot=True)
+    plt.ylabel('True Labels', fontsize=14)
+    plt.xlabel('Predictions', fontsize=14)
+    plt.title('Decision Tree Confusion Matrix')
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+    plt.show()    
 
-# =======================
-# =======特征选择========
-# =======================
+    # 3. SVM
+    SVM = svm.LinearSVC(dual=False, max_iter=2000, random_state=42)
+    SVM.fit(X_train, y_train)
+    # Evaluating the model
+    svm_scores = cross_validate(SVM, X_train, y_train, scoring=['precision_macro', 'recall_macro', 'f1_macro', 'accuracy'])
+    print('svm_accuracy: ', svm_scores['test_accuracy'].mean())
+    print('svm_macro_precision: ', svm_scores['test_precision_macro'].mean())
+    print('svm_macro_recall: ', svm_scores['test_recall_macro'].mean())
+    print('svm_macro_f1_score: ', svm_scores['test_f1_macro'].mean())
+    print('\n')
+    # Obtaining the confusion matrix
+    svm_conf_matrix = confusion_matrix(y_test, SVM.predict(X_test))
+    svm_conf_matrix = pd.DataFrame(svm_conf_matrix, index=['Normal', 'DOS', 'Probing', 'R2L', 'U2R'], columns=['Normal', 'DOS', 'Probing', 'R2L', 'U2R'])  
+    sns.heatmap(svm_conf_matrix, annot=True)
+    plt.ylabel('True Labels', fontsize=14)
+    plt.xlabel('Predictions', fontsize=14)
+    plt.title('SVM Confusion Matrix')
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+    plt.show()       
 
-# 方法1：截距项
-X = pd.DataFrame(X)
-name = X.columns
-X = np.matrix(X)
-print(X)
-VIF_list = [variance_inflation_factor(X,i) for i in range(X.shape[1])]
-VIF = pd.DataFrame({'feature':name,"VIF":VIF_list})
-print(VIF)
-# 方差膨胀系数VIF大于10时表示特征之间有较高的共线性
-
-# 方法2：person相关系数
-X = pd.DataFrame(X)
-corr_df = X.corr()
-# 热力图
-sns.heatmap(corr_df)
-plt.show()
-# 剔除相关性系数高于threshold的corr_drop
-threshold = 0.9
-upper = corr_df.where(np.triu(np.ones(corr_df.shape), k=1).astype(np.bool_))
-corr_drop = [column for column in upper.columns if any(upper[column].abs() > threshold)]
-
-
-'''Training different models'''
-# 1. KNN
-KNN = KNeighborsClassifier(n_neighbors=2, algorithm='kd_tree')
-KNN.fit(X_train, y_train)
-# Evaluating the model
-print('knn_accuracy: ', accuracy_score(y_test, KNN.predict(X_test)))
-print('knn_macro_precision: ', precision_score(y_test, KNN.predict(X_test), average='macro'))
-print('knn_macro_recall: ', recall_score(y_test, KNN.predict(X_test), average='macro'))
-print('knn_macro_f1_score: ', f1_score(y_test, KNN.predict(X_test), average='macro'))
-print('\n')
-# Obtaining the confusion matrix
-knn_conf_matrix = confusion_matrix(y_test, KNN.predict(X_test))
-knn_conf_matrix = pd.DataFrame(knn_conf_matrix, index=['Normal', 'DOS', 'Probing', 'R2L', 'U2R'], columns=['Normal', 'DOS', 'Probing', 'R2L', 'U2R']) 
-sns.heatmap(knn_conf_matrix, annot=True)
-plt.ylabel('True Labels', fontsize=14)
-plt.xlabel('Predictions', fontsize=14)
-plt.title('KNN Confusion Matrix')
-plt.xticks(fontsize=14)
-plt.yticks(fontsize=14)
-plt.show()
-
-# 2. Decision Tree
-Decision_Tree = DecisionTreeClassifier(criterion='entropy', random_state=42, max_depth=5000)
-Decision_Tree.fit(X_train, y_train)
-# Evaluating the model
-print('decision_tree_accuracy: ', accuracy_score(y_test, Decision_Tree.predict(X_test)))
-print('decision_tree_macro_precision: ', precision_score(y_test, Decision_Tree.predict(X_test), average='macro'))
-print('decision_tree_macro_recall: ', recall_score(y_test, Decision_Tree.predict(X_test), average='macro'))
-print('decision_tree_macro_f1_score: ', f1_score(y_test, Decision_Tree.predict(X_test), average='macro'))
-print('\n')
-# Obtaining the confusion matrix
-decision_tree_conf_matrix = confusion_matrix(y_test, Decision_Tree.predict(X_test))
-decision_tree_conf_matrix = pd.DataFrame(decision_tree_conf_matrix, index=['Normal', 'DOS', 'Probing', 'R2L', 'U2R'], columns=['Normal', 'DOS', 'Probing', 'R2L', 'U2R'])  
-sns.heatmap(decision_tree_conf_matrix, annot=True)
-plt.ylabel('True Labels', fontsize=14)
-plt.xlabel('Predictions', fontsize=14)
-plt.title('Decision Tree Confusion Matrix')
-plt.xticks(fontsize=14)
-plt.yticks(fontsize=14)
-plt.show()    
-
-# 3. SVM
-SVM = svm.LinearSVC(dual=False, max_iter=2000, random_state=42)
-SVM.fit(X_train, y_train)
-# Evaluating the model
-print('svm_accuracy: ', accuracy_score(y_test, SVM.predict(X_test)))
-print('svm_macro_precision: ', precision_score(y_test, SVM.predict(X_test), average='macro'))
-print('svm_macro_recall: ', recall_score(y_test, SVM.predict(X_test), average='macro'))
-print('svm_macro_f1_score: ', f1_score(y_test, SVM.predict(X_test), average='macro'))
-print('\n')
-# Obtaining the confusion matrix
-svm_conf_matrix = confusion_matrix(y_test, SVM.predict(X_test))
-svm_conf_matrix = pd.DataFrame(svm_conf_matrix, index=['Normal', 'DOS', 'Probing', 'R2L', 'U2R'], columns=['Normal', 'DOS', 'Probing', 'R2L', 'U2R'])  
-sns.heatmap(svm_conf_matrix, annot=True)
-plt.ylabel('True Labels', fontsize=14)
-plt.xlabel('Predictions', fontsize=14)
-plt.title('SVM Confusion Matrix')
-plt.xticks(fontsize=14)
-plt.yticks(fontsize=14)
-plt.show()       
-
-# 4. Random Forest
-Random_Forest = RandomForestClassifier(n_estimators=100, criterion='entropy', max_depth=3000)
-Random_Forest.fit(X_train, y_train)
-# Evaluating the model
-print('random_forest_accuracy: ', accuracy_score(y_test, Random_Forest.predict(X_test)))
-print('random_forest_macro_precision: ', precision_score(y_test, Random_Forest.predict(X_test), average='macro'))
-print('random_forest_macro_recall: ', recall_score(y_test, Random_Forest.predict(X_test), average='macro'))
-print('random_forest_macro_f1_score: ', f1_score(y_test, Random_Forest.predict(X_test), average='macro'))
-# Obtaining the confusion matrix
-random_forest_conf_matrix = confusion_matrix(y_test, Random_Forest.predict(X_test))
-random_forest_conf_matrix = pd.DataFrame(random_forest_conf_matrix, index=['Normal', 'DOS', 'Probing', 'R2L', 'U2R'], columns=['Normal', 'DOS', 'Probing', 'R2L', 'U2R'])
-sns.heatmap(random_forest_conf_matrix, annot=True)
-plt.ylabel('True Labels', fontsize=14)
-plt.xlabel('Predictions', fontsize=14)
-plt.title('Random Forest Confusion Matrix')
-plt.xticks(fontsize=14)
-plt.yticks(fontsize=14)
-plt.show()
+    # 4. Random Forest
+    Random_Forest = RandomForestClassifier(n_estimators=100, criterion='entropy', max_depth=3000)
+    Random_Forest.fit(X_train, y_train)
+    # Evaluating the model
+    random_forest_scores = cross_validate(Random_Forest, X_train, y_train, scoring=['precision_macro', 'recall_macro', 'f1_macro', 'accuracy'])
+    print('random_forest_accuracy: ', random_forest_scores['test_accuracy'].mean())
+    print('random_forest_macro_precision: ', random_forest_scores['test_precision_macro'].mean())
+    print('random_forest_macro_recall: ', random_forest_scores['test_recall_macro'].mean())
+    print('random_forest_macro_f1_score: ', random_forest_scores['test_f1_macro'].mean())
+    # Obtaining the confusion matrix
+    random_forest_conf_matrix = confusion_matrix(y_test, Random_Forest.predict(X_test))
+    random_forest_conf_matrix = pd.DataFrame(random_forest_conf_matrix, index=['Normal', 'DOS', 'Probing', 'R2L', 'U2R'], columns=['Normal', 'DOS', 'Probing', 'R2L', 'U2R'])
+    sns.heatmap(random_forest_conf_matrix, annot=True)
+    plt.ylabel('True Labels', fontsize=14)
+    plt.xlabel('Predictions', fontsize=14)
+    plt.title('Random Forest Confusion Matrix')
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+    plt.show()
